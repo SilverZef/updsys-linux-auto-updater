984df0b83750d02f96d857e2a82b7e6d456323bd
Silver <123595055+SilverZef@users.noreply.github.com>
2023-01-27 21:58:48 +0530

    Update updsys.sh

--- updsys.sh
+++ updsys.sh
@@ -1,54 +1,40 @@
-LogDisplay="/home/silveros/LogDisplay"
+LogDisplay="/home/silveros/Scr/UPDSYS_Final/LogDisplay"
 
 col1="\e[38;2;148;23;226m"		#purple by default 
 col2="\e[38;2;65;220;76m"		#green by default
 col3="\e[38;2;255;165;0m"		#orange by default(used for errors)
 res="\e[0m"
-path="Log.txt"
 
-#setting path as unused txt file it is allows multiple iterations of this command to run in the same dir
-declare -i index=0
-while [ -e "$path" ]
-do
-	path="Log_$index.txt"
-	index+=1
-done
+path="Log.txt"
 
 Remove() #saves/removes log file after updates, log file is used for progress report or error checking
 {
-    ch=""
-    printf "\e[7;1H\e[0J${col1}Delete $1 Log File ? (y) [y/n/h]: "
-    read ch
-    ch=${ch,}  #Lower Casing ch
+	clear
+	ch=""
+	printf "${col1}Delete $1 Log File ? (y) [y/n/h]: $res"
+	read ch
+	ch=${ch,}  #Lower Casing ch
 
-    if [[ "$ch" == "" || "$ch" == "y" ]]
-    then
-        rm "$path" 
-    elif [[ "$ch" == "h" ]]
-    then
-        printf "$col1\nThe Default is y therefore Entering Nothing, y or Y will Remove $path"
-        printf "\nEntering n or N will NOT Remove $path and will Save the Log File as $1_$path"
-        printf "\nEntering h or H will Print this"
-        sleep 6
-        Remove "$1"		 #calls remove again to ask, i.e it keeps asking until a valid argument is entered
-        #as it is if-elif no return required but if expanded upon please add return after each recursive call
-    elif [[ "$ch" == "n" ]]
-    then
-    	mv "$path" "$1_$path"
-    else
-        printf "$col3\nInvalid Option See Options Within () or Enter h or H for Valid Options\n"
-        sleep 3
-        Remove "$1"
-    fi
+	if [[ "$ch" == "" || "$ch" == "y" ]] #as it is if-elif no return required but if expanded upon please add return after each recursive call
+	then
+		rm "$1_$path" 
+	elif [[ "$ch" == "h" ]]
+	then
+		printf "$col1\nThe Default is y therefore Entering Nothing, y or Y will Remove $path"
+		printf "\nEntering n or N will NOT Remove $path and will Save the Log File as $1_$path"
+		printf "\nEntering h or H will Print this"
+		sleep 6
+		Remove "$1"		 #calls remove again to ask, i.e it keeps asking until a valid argument is entered
+	elif [[ "$ch" != "n" ]]
+	then
+		printf "$col3\nInvalid Option See Options Within () or Enter h or H for Valid Options\n"
+		sleep 3
+		Remove "$1"
+	fi
 }
 
-Execute() #executes commands for repo updates
+Update() #executes commands for repo updates
 {
-	if [ "$1" == "" ]
-	then
-		return 1;
-	fi
-
 	clear    
 
 	comm1="sudo zypper refresh"
@@ -59,28 +45,43 @@ Execute() #executes commands for repo updates
 		printf "\e[1;1f${col2}Status:$col1 Zypper Refresh Begun!"
 		printf "\e[5;1f${col2}Running: $col1$comm1"
 		printf "\e[7;1f${col3}ERRORS: \e[8;1f"
-		$comm1 > "$path"
+		$comm1 > "$1_$path"
 		clear
 		printf "\e[1;1f${col2}Status:$col1 Zypper Upgrade Begun!"
 		printf "\e[5;1f${col2}Running: $col1$comm2"
 		printf "\e[7;1f${col3}ERRORS: \e[8;1f"
-		$comm2 >> "$path"
+		$comm2 >> "$1_$path"
 		printf "\e[1;1f${col2}Status:$col1 Finished Zypper Upgrade!!\e[8;1f"
 	elif [[ "$1" == "Flatpak" ]]
 	then
 		printf "\e[1;1f${col2}Status:$col1 Flatpak Update Begun!"
 		printf "\e[5;1f${col2}Running: $col1$comm3"
 		printf "\e[7;1f${col3}ERRORS: \e[8;1f"
-		$comm3 > "$path"
+		$comm3 > "$1_$path"
 		printf "\e[1;1f${col2}Status:$col1 Finished Flatpak Update !!\e[8;1f"
 	fi
-	echo -e "\nUPGRADE_FINISHED" >> "$path" #USED AS A SIGNAL FOR LOGDISPLAY TO EXIT
+	echo -e "\nUPGRADE_FINISHED" >> "$1_$path" #USED AS A SIGNAL FOR LOGDISPLAY TO EXIT
+}
+
+Run() #Calls the neccessary functions for each updates
+{
+	declare -i index=0 #makes sure a new file is created to not erase previous update logs
+	while [ -e "$1_$path" ]
+	do
+		path="Log_$index.txt"
+		index+=1
+	done
+	
+	Update "$1" &
+	$LogDisplay "$1_$path" &
+	wait
 }
 
 killgroup()#function used to kill all running commands and scripts
 {
 	echo -e "${col3}EXITING ... $res"
-	rm "$path"
+	rm "Zypper_$path"
+	rm "Flatpak_$path"
 	kill 0
 }
 
@@ -119,26 +120,26 @@ then
 	exit 1
 fi
 
+clear
+
 printf "${col1}Super User Access Required Enter Password: $col2\n"
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 
-clear
-
 if [[ "$arg1" != "" ]]
 then
-	Execute "$arg1" &
-	$LogDisplay "$path" &
-	wait
+	Run "$arg1"
+fi
+if [[ "$arg2" != "" ]]
+then
+	Run "$arg2"
+fi
+if [[ "$arg1" != "" ]]
+then
 	Remove "$arg1"
 fi
 if [[ "$arg2" != "" ]]
 then
-	Execute "$arg2" & 
-	$LogDisplay "$path" &
-	wait
 	Remove "$arg2"
 fi
 
-clear
-printf "$res" #Safety Reset Makes Sure No ANSI Sequence is Active Post Upgrades
 exit 0
